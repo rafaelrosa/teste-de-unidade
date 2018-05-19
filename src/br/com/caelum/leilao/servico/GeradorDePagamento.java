ba7d9d59e8dafa339ffac808a5ede88e5e4a522c
Rafael Rosa <rafaelfrancorosa@hotmail.com>
2018-05-19 19:01:09 -0300

    Inclusion of clock object in constructor; method of first working day

--- src/br/com/caelum/leilao/servico/GeradorDePagamento.java
+++ src/br/com/caelum/leilao/servico/GeradorDePagamento.java
@@ -7,16 +7,25 @@ import br.com.caelum.leilao.dominio.Leilao;
 import br.com.caelum.leilao.dominio.Pagamento;
 import br.com.caelum.leilao.infra.dao.RepositorioDeLeiloes;
 import br.com.caelum.leilao.infra.dao.RepositorioDePagamentos;
+import br.com.caelum.leilao.infra.relogio.Relogio;
+import br.com.caelum.leilao.infra.relogio.RelogioDoSistema;
 
 public class GeradorDePagamento {
 	private RepositorioDeLeiloes leiloes;
 	private Avaliador avaliador;
 	private RepositorioDePagamentos pagamentos;
+	private Relogio relogio;
 
-	public GeradorDePagamento(RepositorioDeLeiloes leiloes, RepositorioDePagamentos pagamentos, Avaliador avaliador) {
+	public GeradorDePagamento(RepositorioDeLeiloes leiloes, RepositorioDePagamentos pagamentos, Avaliador avaliador, Relogio relogio) {
 		this.leiloes = leiloes;
 		this.pagamentos = pagamentos;
 		this.avaliador = avaliador;
+		this.relogio = relogio;
+	}
+	
+	// for compatibility issues
+	public GeradorDePagamento(RepositorioDeLeiloes leiloes, RepositorioDePagamentos pagamentos, Avaliador avaliador) {
+		this(leiloes, pagamentos, avaliador, new RelogioDoSistema());
 	}
 	
 	public void gera() {
@@ -25,8 +34,19 @@ public class GeradorDePagamento {
 		for(Leilao leilao: leiloesEncerrados) {
 			this.avaliador.avalia(leilao);
 			
-			Pagamento novoPagamento = new Pagamento(avaliador.getMaiorLance(), Calendar.getInstance());
+			Pagamento novoPagamento = new Pagamento(avaliador.getMaiorLance(), primeiroDiaUtil());
 			this.pagamentos.salva(novoPagamento);
 		}
+	}
+
+	private Calendar primeiroDiaUtil() {
+		Calendar data = relogio.hoje();
+		int diaDaSemana = data.get(Calendar.DAY_OF_WEEK);
+		
+		// if day is on weekend, consider closest Monday.
+		if(diaDaSemana == Calendar.SATURDAY) data.add(Calendar.DAY_OF_MONTH, 2);
+		else if(diaDaSemana == Calendar.SUNDAY) data.add(Calendar.DAY_OF_MONTH, 1);
+		
+		return data;
 	}
 }
